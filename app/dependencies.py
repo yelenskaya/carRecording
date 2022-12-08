4c94151cba0637bfec54dd7c1d4306bd54f5e1fa
yelenskaya <v.yelenskaya@gmail.com>
2022-12-08 14:07:25 +0200

    Get all recordings

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 
-from boto3 import client
+from boto3 import resource
+from fastapi import Depends
 
 from app.settings import Settings
 
@@ -11,5 +12,10 @@ def get_settings():
 
 
 @lru_cache
-def get_s3_client():
-    return client('s3')
+def get_s3_resource():
+    return resource('s3')
+
+
+@lru_cache
+def get_s3_bucket(s3_resource=Depends(get_s3_resource), settings=Depends(get_settings)):
+    return s3_resource.Bucket(settings.S3_BUCKET)
