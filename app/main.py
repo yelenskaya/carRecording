ed50e944e0b757fc34f7fc5241ce9062d32a01c3
yelenskaya <v.yelenskaya@gmail.com>
2022-12-19 19:25:56 +0200

    Socket namespace

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from socketio import ASGIApp
+from socketio import ASGIApp, AsyncServer
 
-from app.events import socket_server
+from app.events import recording_namespace
 from app.routes import router
 
 app = FastAPI()
 app.include_router(router)
 
+socket_server = AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 socket_app = ASGIApp(socket_server, app)
 app.mount('/ws', socket_app)
+
+socket_server.register_namespace(recording_namespace)
